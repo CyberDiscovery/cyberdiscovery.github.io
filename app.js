f87c242073195183011050312aecf7b4e943c046
Yudhajit N <28938427+Sh3llcod3@users.noreply.github.com>
2020-05-20 03:23:11 +0100

    Finished homepage, other tabs left.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
 const topAppBarMenu = new MDCMenu(document.querySelector('#additional-menu'));
 const discordURL = "https://discord.cyberdiscoverycommunity.uk";
 const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
-const selector = '.mdc-button, .mdc-icon-button, .mdc-list-item';
+const selector = '.mdc-button, .mdc-list-item';
 const copyLinkBtn = document.querySelector('#copy-site-link');
 const discordInvBtn = document.querySelector('#join-server-btn');
 const additionalMenuBtn = document.querySelector('#additional-menu-btn');
@@ -119,6 +119,12 @@ const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
   return new MDCRipple(el);
 });
 
+const btnRipple = [].map.call(document.querySelectorAll(".mdc-icon-button"), function(el) {
+  const ripple = new MDCRipple(el);
+  ripple.unbounded = true;
+  return ripple;
+});
+
 discordInvBtn.addEventListener('click', function(event) {
   openLinkHandler(discordURL);
 });
@@ -126,3 +132,33 @@ additionalMenuBtn.addEventListener('click', function(event) {topAppBarMenu.open 
 document.querySelector("#switchThemeMenu").addEventListener('click', function(event) {displayNotYetImplemented();});
 document.querySelector("#copySiteLnkMenu").addEventListener('click', function(event) {copySiteLinkToClipboard();});
 document.querySelector("#discordInvMenu").addEventListener('click', function(event) {openLinkHandler(discordURL);});
+
+document.querySelector("#bot-repo-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cyberdisc-bot");});
+document.querySelector("#bot-star-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cyberdisc-bot/stargazers");});
+document.querySelector("#bot-fork-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/login?return_to=%2FCyberDiscovery%2Fcyberdisc-bot");});
+document.querySelector("#bot-issue-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cyberdisc-bot/issues");});
+
+document.querySelector("#site-repo-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cyberdiscovery.github.io");});
+document.querySelector("#site-star-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cyberdiscovery.github.io/stargazers");});
+document.querySelector("#site-fork-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/login?return_to=%2FCyberDiscovery%2Fcyberdiscovery.github.io");});
+document.querySelector("#site-issue-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cyberdiscovery.github.io/issues");});
+
+document.querySelector("#app-repo-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/Cyber-Discovery-App");});
+document.querySelector("#app-star-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/Cyber-Discovery-App/stargazers");});
+document.querySelector("#app-fork-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/login?return_to=%2FCyberDiscovery%2FCyber-Discovery-App");});
+document.querySelector("#app-issue-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/Cyber-Discovery-App/issues");});
+
+document.querySelector("#math-repo-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/Discord-Maths-Bot");});
+document.querySelector("#math-star-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/discord-maths-bot/stargazers");});
+document.querySelector("#math-fork-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/login?return_to=%2FCyberDiscovery%2Fdiscord-maths-bot");});
+document.querySelector("#math-issue-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/Discord-Maths-Bot/issues");});
+
+document.querySelector("#elect-repo-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/elections-bot");});
+document.querySelector("#elect-star-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/elections-bot/stargazers");});
+document.querySelector("#elect-fork-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/login?return_to=%2FCyberDiscovery%2Felections-bot");});
+document.querySelector("#elect-issue-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/elections-bot/issues");});
+
+document.querySelector("#rich-repo-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cd-rich-presence");});
+document.querySelector("#rich-star-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cd-rich-presence/stargazers");});
+document.querySelector("#rich-fork-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/login?return_to=%2FCyberDiscovery%2Fcd-rich-presence");});
+document.querySelector("#rich-issue-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cd-rich-presence/issues");});
