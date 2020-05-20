2f0ce5c563acdf32329c15647d59707361ad0dc5
Yudhajit N <28938427+Sh3llcod3@users.noreply.github.com>
2020-05-21 00:05:32 +0100

    Finished tab system and contributors page - Added memes in dev - Oh wait I dev in prod - Hmmm

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ import {MDCSnackbar} from '@material/snackbar';
 import {MDCRipple} from '@material/ripple';
 import {MDCMenu} from '@material/menu';
 import {MDCMenuSurface} from '@material/menu-surface';
+import {MDCDataTable} from '@material/data-table';
 
 const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
 const topAppBarMenu = new MDCMenu(document.querySelector('#additional-menu'));
 const discordURL = "https://discord.cyberdiscoverycommunity.uk";
 const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
-const selector = '.mdc-button, .mdc-list-item';
+const selector = '.mdc-button, .mdc-list-item, .mdc-fab';
 const copyLinkBtn = document.querySelector('#copy-site-link');
 const discordInvBtn = document.querySelector('#join-server-btn');
 const additionalMenuBtn = document.querySelector('#additional-menu-btn');
@@ -19,6 +20,11 @@ const listEl = document.querySelector('.mdc-drawer .mdc-list');
 const drawerElement = document.querySelector('.mdc-drawer');
 const mainContentEl = document.querySelector('.main-content');
 const additionalMenuSurface = new MDCMenuSurface(document.querySelector('#additional-menu'));
+const pgTopBtn = document.querySelector("#scrollPgTop");
+const siteMainTitle = "CD Discord Community | ";
+const hideTab = "site-tab-hidden";
+//const dataTable = new MDCDataTable(document.querySelector('#site-contrib-table'));
+const siteContribList = new MDCList(document.querySelector('#site-contrib-list'));
 const initModalDrawer = () => {
   drawerElement.classList.add("mdc-drawer--modal");
   const drawer = MDCDrawer.attachTo(drawerElement);
@@ -125,6 +131,102 @@ const btnRipple = [].map.call(document.querySelectorAll(".mdc-icon-button"), fun
   return ripple;
 });
 
+function detectPageBottom() {
+  if ((window.innerHeight + window.pageYOffset) >= document.body.scrollHeight - 64) {
+    pgTopBtn.classList.remove("mdc-fab--exited");
+  } else {
+    pgTopBtn.classList.add("mdc-fab--exited");
+  }
+}
+
+let previousTab = document.querySelector("#site-home-tab");
+
+function changeTab(tabID) {
+  //
+  switch (tabID) {
+    case "opt0":
+      document.title = siteMainTitle + "Home";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-home-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt1":
+      document.title = siteMainTitle + "Soundboard";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-soundboard-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt2":
+      document.title = siteMainTitle + "Upcoming Events";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-dates-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt3":
+      document.title = siteMainTitle + "Code Of Conduct";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-coc-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt4":
+      document.title = siteMainTitle + "Resources";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-resources-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt5":
+      document.title = siteMainTitle + "Contributing";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-Contributing-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt6":
+      document.title = siteMainTitle + "Activity Nights";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-activity-nights-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt7":
+      document.title = siteMainTitle + "Server Roles";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-roles-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt8":
+      document.title = siteMainTitle + "Frequently Asked Questions";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-faq-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt9":
+      document.title = siteMainTitle + "Disclaimer";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-disclaimer-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    case "opt10":
+      document.title = siteMainTitle + "Site Contributors";
+      previousTab.classList.add(hideTab);
+      previousTab = document.querySelector("#site-whois-tab");
+      previousTab.classList.remove(hideTab);
+      break;
+
+    default:
+      break;
+  }
+  //
+}
+
 discordInvBtn.addEventListener('click', function(event) {
   openLinkHandler(discordURL);
 });
@@ -162,3 +264,18 @@ document.querySelector("#rich-repo-btn").addEventListener('click', function(even
 document.querySelector("#rich-star-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cd-rich-presence/stargazers");});
 document.querySelector("#rich-fork-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/login?return_to=%2FCyberDiscovery%2Fcd-rich-presence");});
 document.querySelector("#rich-issue-btn").addEventListener('click', function(event) {openLinkHandler("https://github.com/CyberDiscovery/cd-rich-presence/issues");});
+
+document.querySelector("#aboutPageMenu").addEventListener('click', function(event) {changeTab("opt10");});
+document.querySelector("#eo-gh-page").addEventListener('click', function(event) {openLinkHandler("https://github.com/eohomegrownapps");});
+document.querySelector("#ana-gh-page").addEventListener('click', function(event) {openLinkHandler("https://github.com/lightspeedana/");});
+document.querySelector("#pica-gh-page").addEventListener('click', function(event) {openLinkHandler("https://github.com/picapi");});
+document.querySelector("#butters-gh-page").addEventListener('click', function(event) {openLinkHandler("https://github.com/bottersnike");});
+document.querySelector("#beano-gh-page").addEventListener('click', function(event) {openLinkHandler("https://github.com/thebeanogamer");});
+document.querySelector("#segway-gh-page").addEventListener('click', function(event) {openLinkHandler("https://github.com/fractalpenguin");});
+document.querySelector("#bac-gh-page").addEventListener('click', function(event) {openLinkHandler("https://github.com/ZomBMage");});
+document.querySelector("#linucks-gh-page").addEventListener('click', function(event) {openLinkHandler("https://github.com/sh3llcod3");});
+
+pgTopBtn.addEventListener('click', function(event) {goToPageTop();});
+// window.addEventListener("scroll", detectPageBottom);
+
+drawer.listen('click', function(event) {changeTab(event.target.id)}, false);
